ad3c45c5639a4d8b572c8a1be44dff8eb1f0a31f
jdbandoniml <jonathan.bandoni@mercadolibre.com>
2020-09-05 10:19:50 -0300

    configuración inicial de podfile y proyecto

---
+++ Cocina/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  Cocina
+//
+//  Created by JONATHAN DANIEL BANDONI on 05/09/2020.
+//  Copyright © 2020 JONATHAN DANIEL BANDONI. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        
+        let navController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Cocina' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Cocina
+
+  target 'CocinaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end

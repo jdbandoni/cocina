92661c00cfe44994773d126595a350adfd050f4e
jdbandoniml <jonathan.bandoni@mercadolibre.com>
2020-09-05 10:50:46 -0300

    tableview y row para enlistar

---
+++ Cocina/HomePresenter.swift
@@ -0,0 +1,13 @@
+//
+//  HomePresenter.swift
+//  Cocina
+//
+//  Created by JONATHAN DANIEL BANDONI on 05/09/2020.
+//  Copyright © 2020 JONATHAN DANIEL BANDONI. All rights reserved.
+//
+
+import Foundation
+
+class HomePresenter {
+    
+}
---
+++ Cocina/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  Cocina
+//
+//  Created by JONATHAN DANIEL BANDONI on 05/09/2020.
+//  Copyright © 2020 JONATHAN DANIEL BANDONI. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
